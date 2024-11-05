027bc50435f656ca3d2e6be3e9ca3bb609f927f9
AlexTheMaggot <alexthemaggot23@gmail.com>
2024-11-05 11:21:53 +0500

    - create state model

--- tuft_lac/api/models.py
+++ tuft_lac/api/models.py
@@ -3,3 +3,7 @@ from django.db import models
 
 class Machine(models.Model):
     name = models.CharField(max_length=20, unique=True)
+
+
+class State(models.Model):
+    name = models.CharField(max_length=20, unique=True)
