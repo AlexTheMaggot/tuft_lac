9b0d30befb3ed61c164c35bfb82196ae6f84dff9
AlexTheMaggot <alexthemaggot23@gmail.com>
2024-11-05 16:11:27 +0500

    - add index view

--- tuft_lac/api/views.py
+++ tuft_lac/api/views.py
@@ -1,6 +1,26 @@
+import json
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponseBadRequest
+from django.core.exceptions import ObjectDoesNotExist
+from django.views.decorators.csrf import csrf_exempt
+from .models import *
 
-
+@csrf_exempt
 def index(request):
-    return JsonResponse({'result': 'Success'})
+    try:
+        data = json.loads(request.body)
+    except json.JSONDecodeError:
+        return HttpResponseBadRequest(content='Invalid JSON')
+    if 'name' in data.keys() and 'state' in data.keys():
+        try:
+            record = Record.objects.create(machine=Machine.objects.get(name=data['name']))
+            states = []
+            for i in data['state']:
+                record.states.add(State.objects.get(name=i))
+            record.save()
+            return JsonResponse({'result': 'Success'})
+        except ObjectDoesNotExist:
+            return HttpResponseBadRequest(content='Wrong name or state')
+    else:
+        return HttpResponseBadRequest(content='Missing name or state')
+
