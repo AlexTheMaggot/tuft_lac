18d7105a04d971244aadf48aec69c191199b4747
AlexTheMaggot <alexthemaggot23@gmail.com>
2024-11-05 11:51:34 +0500

    - add str methods to state record and machine models

--- tuft_lac/api/models.py
+++ tuft_lac/api/models.py
@@ -4,12 +4,21 @@ from django.db import models
 class Machine(models.Model):
     name = models.CharField(max_length=20, unique=True)
 
+    def __str__(self):
+        return self.name
+
 
 class State(models.Model):
     name = models.CharField(max_length=20, unique=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Record(models.Model):
     machine = models.ForeignKey(Machine, on_delete=models.CASCADE)
     states = models.ManyToManyField(State, null=True, blank=True)
     datetime = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.datetime)
