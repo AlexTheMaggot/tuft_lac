8cfbf3bdc2f6f39b72fd903155e7ccf0afe3b0aa
AlexTheMaggot <alexthemaggot23@gmail.com>
2024-12-02 14:16:14 +0500

    - fix admin appearance

--- tuft_lac/api/admin.py
+++ tuft_lac/api/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from .models import *
 
 
+class RecordAdmin(admin.ModelAdmin):
+    list_display = ['machine', 'get_states', 'datetime']
+
+
 admin.site.register(Machine)
 admin.site.register(State)
-admin.site.register(Record)
+admin.site.register(Record, RecordAdmin)
