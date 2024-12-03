90ff7dcddc6d38d6b6a8b47b2216fa75feb1a2cb
AlexTheMaggot <alexthemaggot23@gmail.com>
2024-12-03 12:29:54 +0500

    - add dynamic pics and status

--- tuft_lac/mainapp/views.py
+++ tuft_lac/mainapp/views.py
@@ -23,12 +23,33 @@ def index(request):
             productivity = worktime.seconds * 100 // (worktime + freetime).seconds
         else:
             productivity = 0
+        connected = False
+        if m.records.last():
+            td = datetime.datetime.now(tz=datetime.timezone.utc) - m.records.last().updated
+            if td.seconds < 10:
+                connected = True
+        pic = 'NC.png'
+        if m.records.last():
+            if not {i.name for i in m.records.last().states.all()}:
+                pic = 'IDL'
+            elif {i.name for i in m.records.last().states.all()} == {'red'}:
+                pic = 'R'
+            elif {i.name for i in m.records.last().states.all()} == {'red', 'green'}:
+                pic = 'RG'
+            elif {i.name for i in m.records.last().states.all()} == {'red', 'yellow'}:
+                pic = 'RY'
+            elif {i.name for i in m.records.last().states.all()} == {'red', 'green', 'yellow'}:
+                pic = 'RYG'
+            elif {i.name for i in m.records.last().states.all()} == {'yellow', 'green'}:
+                pic = 'YG'
         result.append({
             'id': m.id,
             'worktime': worktime.seconds // 3600,
             'freetime': freetime.seconds // 3600,
             'count': m.records.count(),
             'productivity': productivity,
+            'connected': connected,
+            'pic': pic,
         })
     context = {
         'machines': result,
