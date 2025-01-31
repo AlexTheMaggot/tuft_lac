3956a9272419171ca062194001e1a0db6c554b3a
AlexTheMaggot <alexthemaggot23@gmail.com>
2025-01-31 13:52:16 +0500

    - add light alert and phone number urls

--- tuft_lac/mainapp/urls.py
+++ tuft_lac/mainapp/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('light_alert/', views.light_alert, name='light_alert'),
+    path('phone_number/', views.phone_number, name='phone_number'),
 ]
